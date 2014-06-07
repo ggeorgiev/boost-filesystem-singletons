579c71f15296c851ee2592f89c464fff5a9d897b
George Georgiev <george.georgiev.sf@gmail.com>
2014-06-07 15:17:07 -0700

    add the crash code

--- filesystem-test/main.cpp
+++ filesystem-test/main.cpp
@@ -1,7 +1,52 @@
-#include <iostream>
+#include "boost/thread.hpp"
+#include "boost/filesystem/path.hpp"
+
+#include <vector>
+
+namespace boost
+{
+
+void assertion_failed(char const * expr, char const * function, char const * file, long line)
+{
+}
+
+}
+
+class Worker
+{
+public:
+    Worker(boost::barrier& barrier) 
+        : mBarrier(barrier)
+    {
+    }
+
+    void operator()()
+    {
+        mBarrier.wait();
+        boost::filesystem::path path("foo");
+    }
+
+    boost::barrier& mBarrier;
+};
 
 int main(int argc, char **argv)
 {
-    std::cout << "Do nothing.";
-   return 0;
+    // uncomment this to workaround the global object initialization problem
+    //boost::filesystem::path path("foo");
+
+    int count = 10;
+    boost::barrier barrier(count + 1);
+
+    Worker w(barrier);
+
+    std::vector<boost::shared_ptr<boost::thread> > vThread;
+    for (int i = 0; i < count; ++i)
+        vThread.push_back(boost::shared_ptr<boost::thread>(new boost::thread(w)));
+
+    barrier.wait();
+
+    for (int i = 0; i < count; ++i)
+        vThread[i]->join();
+
+    return 0;
 }
